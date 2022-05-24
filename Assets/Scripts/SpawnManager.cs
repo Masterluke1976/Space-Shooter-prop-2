6e8b8ad5eed15400c75d9d1567d634b98392524c
unknown <masterluke76@gmail.com>
2022-05-24 17:12:27 -0400

    Created enemy wave system

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -13,6 +13,15 @@ public class SpawnManager : MonoBehaviour
 
     private bool _stopSpawning = false;
 
+    //5.24 wave system need variable for three waves
+    [SerializeField]
+    private GameObject[] _wave1;
+    [SerializeField]
+    private GameObject[] _wave2;
+    [SerializeField]
+    private GameObject[] _wave3;
+
+    private int _currentWave = 1;
     
     
 
@@ -36,15 +45,56 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnEnemyRoutine()
     {
+        Vector3 postToSpawn;
         yield return new WaitForSeconds(3.0f);
-
+        // 5.24
         while (_stopSpawning == false)
         {
+            //switch statements for waves
+            switch (_currentWave)
+            {
+                case 1:
+                    
+                    foreach (GameObject enemy in _wave1)
+                    {
+                        postToSpawn = new Vector3(Random.Range(-8f, 8f), 10, 0);
+                        GameObject newEnemy = Instantiate(enemy, postToSpawn, Quaternion.identity);
+                        newEnemy.transform.parent = _enemyContainer.transform;
+                        yield return new WaitForSeconds(5.0f);
+                    }
+                    break;
+                case 2:
+                    
+                    foreach (GameObject enemy in _wave2)
+                    {
+                        postToSpawn = new Vector3(Random.Range(-8f, 8f), 10, 0);
+                        GameObject newEnemy = Instantiate(enemy, postToSpawn, Quaternion.identity);
+                        newEnemy.transform.parent = _enemyContainer.transform;
+                        yield return new WaitForSeconds(5.0f);
+                    }
+                    break;
+                case 3:
+                    
+                    foreach (GameObject enemy in _wave3)
+                    {
+                        postToSpawn = new Vector3(Random.Range(-8f, 8f), 10, 0);
+                        GameObject newEnemy = Instantiate(enemy, postToSpawn, Quaternion.identity);
+                        newEnemy.transform.parent = _enemyContainer.transform;
+                        yield return new WaitForSeconds(5.0f);
+                    }
+                    break;
+                default:
+                    Debug.Log("No more Waves");
+                    break;
+
+            }
+            yield return new WaitForSeconds(2);
+            _currentWave++;
+            //Vector3 postToSpawn = new Vector3(Random.Range(-8f, 8f), 10, 0);
+            //GameObject newEnemy = Instantiate(_enemyPrefab, postToSpawn, Quaternion.identity);
+            //newEnemy.transform.parent = _enemyContainer.transform;
+            //yield return new WaitForSeconds(5.0f);
             
-            Vector3 postToSpawn = new Vector3(Random.Range(-8f, 8f), 10, 0);
-            GameObject newEnemy = Instantiate(_enemyPrefab, postToSpawn, Quaternion.identity);
-            newEnemy.transform.parent = _enemyContainer.transform;
-            yield return new WaitForSeconds(5.0f);
         }
         
     }
